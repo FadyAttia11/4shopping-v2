7ac1fbd3fdcc91f1732ca808497229760d48b1b3
FadyAttia <fadyattia11@gmail.com>
2020-12-02 06:03:11 +0200

    commented out static react folder on production

--- server/index.js
+++ server/index.js
@@ -2,14 +2,14 @@ const app = require('./App')
 const path = require('path')
 const express = require('express')
 
-//serve static assets if in production (serve the react app) ==> (have to add heroku-postbuild to package.json)
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// //serve static assets if in production (serve the react app) ==> (have to add heroku-postbuild to package.json)
+// if(process.env.NODE_ENV === 'production'){
+//     //set static folder
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
 
 const port = process.env.PORT || 5000
